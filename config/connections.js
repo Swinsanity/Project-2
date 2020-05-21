d2298682052733224ec556ecab03c6bf7af51506
Evan <evanmgreen063@gmail.com>
2020-05-20 21:11:32 -0400

    connections and schema updated

--- config/connections.js
+++ config/connections.js
@@ -1,1 +1,19 @@
-//connections.js
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+	host: " ",
+	port: 3306,
+	user: " ",
+	password: " ",
+	database: " "
+  });
+
+connection.connect(function(err){
+	if(err){
+		console.log("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
